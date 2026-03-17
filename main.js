ab42d2a0d8d9ce169d64e1458d7f155a993ea4ff
KennethConcepcionKOC <concepcion.ko@obmontessori.edu.ph>
2026-03-17 12:32:56 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Participation Activity 2
+function compute_rank() {
+    let math_score = Number(document.getElementById("math_score").value);
+
+    if (math_score > 94) {
+        window.alert('Gold');
+    } else if (math_score > 89) {
+        window.alert('Silver');  
+    } else if (math_score > 84) {
+        window.alert('Bronze');
+    } else if (math_score > 79) {
+        window.alert('Merit');
+    }
+    else {
+        window.alert("Invalid input");
+    }
+}
