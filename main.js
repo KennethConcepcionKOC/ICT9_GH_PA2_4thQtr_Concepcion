bb3492044a103953a107faaf84abb1783aac2640
KennethConcepcionKOC <concepcion.ko@obmontessori.edu.ph>
2026-03-17 12:42:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 function compute_rank() {
     let math_score = Number(document.getElementById("math_score").value);
 
-    if (math_score > 94) {
+     if (math_score > 100) {
+        window.alert('Invalid input');
+    }
+     else if (math_score > 94) {
         window.alert('Gold');
     } else if (math_score > 89) {
         window.alert('Silver');  
